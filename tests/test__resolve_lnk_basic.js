da6daec9818e39b9a3480f4a4b90bd62c1e85270
Ash <ashbeats@gmail.com>
2020-07-24 00:30:39 +0700

    exceptions and others

--- tests/test__resolve_lnk_basic.js
+++ tests/test__resolve_lnk_basic.js
@@ -3,26 +3,38 @@ const {
   resolve_lnk_basic_from
 } = require("../src/resolve_lnk_basic.js");
 
+const { ResolveLnkException } = require("../src/resolve_lnk_exception.js");
+
 const path = require("path");
 const fs = require("fs");
 
+function setup_helpers() {
+  const test_helpers = {
+    resolve_lnk_basic_from: () => ({
+      expectedOutput:
+        "C:\\ashbeats\\software-source-codes\\2020-projects\\2020-YoutubeReactorsTools",
+      testInput: path.join(__dirname, "sample-lnks/folder-link1.lnk")
+    })
+  };
+  return test_helpers;
+}
+
 describe("Can parse lnks?", () => {
-  
-  const testData = path.join(__dirname, "sample-lnks/folder-link1.lnk");
-  const testDataResult = JSON.parse(
-    fs
-      .readFileSync(
-        path.join(__dirname, "sample-outputs/folder-link1.lnk.json"),
-        {
-          encoding: "utf-8"
-        }
-      )
-      .toString()
-  );
-  
-  
-  test("and extract details from lnk files?", async () => {
-    let data = await resolve_lnk_basic_from(testData);
-    expect(data).toMatch("C:\\ashbeats\\software-source-codes\\2020-projects\\2020-YoutubeReactorsTools");
+  const input = setup_helpers().resolve_lnk_basic_from();
+
+  test("and extract details from lnk buffer", () => {
+    let output = resolve_lnk_basic(fs.readFileSync(input.testInput));
+    expect(output).toMatch(input.expectedOutput);
   });
+
+  test("and extract details from lnk files?", async () => {
+    let output = await resolve_lnk_basic_from(input.testInput);
+    expect(output).toMatch(input.expectedOutput);
+  });
+
+  test("throws ResolveLnkException", () => {
+    expect(() =>  resolve_lnk_basic(Buffer.alloc(32))).toThrow(ResolveLnkException);
+  });
+
+  // todo - test if it
 });
